b9c68e1a7ab3bc7f683d078547e578bc8d0e145a
TusharJaingithub <jaintushar432@gmail>
2026-03-29 17:44:43 +0530

    add buttons for mobile

--- script.js
+++ script.js
@@ -11,16 +11,29 @@ const timeElement = document.querySelector("#time");
 const blockHeight = 50;
 const blockWidth = 50;
 const blocks = [];
+
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+
 let snake = [{ x: 1, y: 3 }];
 let direction = "right";
 let intervalId = null;
 let timerIntervalId = null;
+
 let highScore = localStorage.getItem("highScore") || 0;
 highScoreElement.innerText = highScore;
+
 let score = 0;
 let time = `00:00`;
+
+for (let row = 0; row < rows; row++) {
+  for (let col = 0; col < cols; col++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    board.appendChild(block);
+    blocks[`${row}-${col}`] = block;
+  }
+}
 
 function generateFood() {
   let newFood;
@@ -36,64 +49,49 @@ function generateFood() {
 }
 
 let food = generateFood();
-for (let row = 0; row < rows; row++) {
-  for (let col = 0; col < cols; col++) {
-    const block = document.createElement("div");
-    block.classList.add("block");
-    board.appendChild(block);
-    blocks[`${row}-${col}`] = block;
-  }
+
+function gameOver() {
+  clearInterval(intervalId);
+  clearInterval(timerIntervalId);
+
+  snake.forEach((segment) => {
+    blocks[`${segment.x}-${segment.y}`].classList.remove("fill", "head");
+  });
+
+  modal.style.display = "flex";
+  startGameModal.style.display = "none";
+  gameOverModal.style.display = "flex";
 }
 
 function render() {
   let head = null;
-
-  blocks[`${food.x}-${food.y}`].classList.add("food");
 
   if (direction === "left") head = { x: snake[0].x, y: snake[0].y - 1 };
   else if (direction === "right") head = { x: snake[0].x, y: snake[0].y + 1 };
   else if (direction === "up") head = { x: snake[0].x - 1, y: snake[0].y };
   else if (direction === "down") head = { x: snake[0].x + 1, y: snake[0].y };
 
-  // Wall collision
+  // Wall Collision
   if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols) {
-  clearInterval(intervalId);
-  clearInterval(timerIntervalId);
+    gameOver();
+    return;
+  }
 
-  // clear snake from board
-  snake.forEach((segment) => {
-    blocks[`${segment.x}-${segment.y}`].classList.remove("fill", "head");
-  });
-
-  modal.style.display = "flex";
-  startGameModal.style.display = "none";
-  gameOverModal.style.display = "flex";
-  return;
-}
-
-  // Self collision
+  // Self Collision
   if (snake.some((seg) => seg.x === head.x && seg.y === head.y)) {
-  clearInterval(intervalId);
-  clearInterval(timerIntervalId);
-
-  // clear snake from board
-  snake.forEach((segment) => {
-    blocks[`${segment.x}-${segment.y}`].classList.remove("fill", "head");
-  });
-
-  modal.style.display = "flex";
-  startGameModal.style.display = "none";
-  gameOverModal.style.display = "flex";
-  return;
-}
+    gameOver();
+    return;
+  }
 
   let ateFood = false;
 
-  // Food eat
+  // Food Eat
   if (head.x === food.x && head.y === food.y) {
     blocks[`${food.x}-${food.y}`].classList.remove("food");
 
     food = generateFood();
+    ateFood = true;
+
     score += 10;
     scoreElement.innerText = score;
 
@@ -102,8 +100,6 @@ function render() {
       highScoreElement.innerText = highScore;
       localStorage.setItem("highScore", highScore.toString());
     }
-
-    ateFood = true;
   }
 
   // Clear old snake
@@ -118,7 +114,7 @@ function render() {
     snake.pop();
   }
 
-  // Draw new snake
+  // Draw snake
   snake.forEach((segment, index) => {
     const block = blocks[`${segment.x}-${segment.y}`];
     block.classList.add("fill");
@@ -127,51 +123,16 @@ function render() {
       block.classList.add("head");
     }
   });
+
+  // Draw food
+  blocks[`${food.x}-${food.y}`].classList.add("food");
 }
 
-startButton.addEventListener("click", () => {
-  modal.style.display = "none";
-  intervalId = setInterval(() => {
-    render();
-  }, 300);
-  timerIntervalId = setInterval(() => {
-    let [min, sec] = time.split(":").map(Number);
-    if (sec === 59) {
-      min++;
-      sec = 0;
-    } else {
-      sec++;
-    }
-    time = `${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`;
-    timeElement.innerText = time;
-  }, 1000);
-});
-
-restartButton.addEventListener("click", restartGame);
-
-function restartGame() {
+function startGame() {
   clearInterval(intervalId);
   clearInterval(timerIntervalId);
 
-  blocks[`${food.x}-${food.y}`].classList.remove("food");
-  snake.forEach((segment) => {
-    blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
-  });
-
-  score = 0;
-  scoreElement.innerText = score;
-
-  time = `00:00`;
-  timeElement.innerText = time;
-
-  modal.style.display = "none";
-
-  snake = [{ x: 1, y: 3 }];
-  direction = "right";
-
-  food = generateFood();
-
-  intervalId = setInterval(render, 300);
+  intervalId = setInterval(render, 250);
 
   timerIntervalId = setInterval(() => {
     let [min, sec] = time.split(":").map(Number);
@@ -191,14 +152,63 @@ function restartGame() {
   }, 1000);
 }
 
+startButton.addEventListener("click", () => {
+  modal.style.display = "none";
+  startGameModal.style.display = "flex";
+  gameOverModal.style.display = "none";
+
+  startGame();
+});
+
+restartButton.addEventListener("click", restartGame);
+
+function restartGame() {
+  clearInterval(intervalId);
+  clearInterval(timerIntervalId);
+
+  // clear old snake and food
+  snake.forEach((segment) => {
+    blocks[`${segment.x}-${segment.y}`].classList.remove("fill", "head");
+  });
+
+  blocks[`${food.x}-${food.y}`].classList.remove("food");
+
+  score = 0;
+  scoreElement.innerText = score;
+
+  time = "00:00";
+  timeElement.innerText = time;
+
+  snake = [{ x: 1, y: 3 }];
+  direction = "right";
+  food = generateFood();
+
+  modal.style.display = "none";
+
+  startGame();
+}
+
+// Desktop Keyboard Controls
 window.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowLeft" && direction !== "right") {
-    direction = "left";
-  } else if (e.key === "ArrowRight" && direction !== "left") {
-    direction = "right";
-  } else if (e.key === "ArrowUp" && direction !== "down") {
-    direction = "up";
-  } else if (e.key === "ArrowDown" && direction !== "up") {
-    direction = "down";
-  }
+  if (e.key === "ArrowLeft" && direction !== "right") direction = "left";
+  else if (e.key === "ArrowRight" && direction !== "left") direction = "right";
+  else if (e.key === "ArrowUp" && direction !== "down") direction = "up";
+  else if (e.key === "ArrowDown" && direction !== "up") direction = "down";
+});
+
+// Mobile Controls Buttons
+document.querySelector(".up").addEventListener("click", () => {
+  if (direction !== "down") direction = "up";
+});
+
+document.querySelector(".down").addEventListener("click", () => {
+  if (direction !== "up") direction = "down";
+});
+
+document.querySelector(".left").addEventListener("click", () => {
+  if (direction !== "right") direction = "left";
+});
+
+document.querySelector(".right").addEventListener("click", () => {
+  if (direction !== "left") direction = "right";
 });
